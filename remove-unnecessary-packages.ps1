3fecdd4c18d1bd88ceb998d8c527ef37ec54ca88
wataken44 <wataken44@users.noreply.github.com>
2022-05-15 22:04:33 +0900

    windows 10/11 の不要パッケージ削除スクリプトを追加

---
+++ remove-unnecessary-packages.ps1
@@ -0,0 +1,32 @@
+﻿
+$packages = @(
+    "*CandyCrush*",
+    "*Disney*",
+    "*Zune*",
+    "Clipchamp.Clipchamp",
+    "Microsoft.3DBuilder",
+    "Microsoft.Bing*",
+    "Microsoft.Messaging",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MixedReality.Portal",
+    "Microsoft.OneConnect",
+    "Microsoft.People",
+    "Microsoft.Print3D",
+    "Microsoft.SkypeApp",
+    "Microsoft.Todos",
+    "Microsoft.WindowsAlarms",
+    "Microsoft.WindowsMaps",
+    "Microsoft.Xbox*Overlay",
+    "Microsoft.YourPhone",
+    "SpotifyAB.SpotifyMusic",
+    "NORDCURRENT.COOKINGFEVER",
+    "Microsoft.Office.OneNote",
+    "Fitbit.FitbitCoach",
+    "microsoft.windowscommunicationsapps"
+
+)
+
+foreach ($pkg in $packages) {
+    Write-Output $pkg
+    Get-AppxPackage -AllUsers $pkg | Remove-AppxPackage -AllUsers
+}
